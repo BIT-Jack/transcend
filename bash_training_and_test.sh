17322c63dd46034151df60fd185feaee0e8fb794
BIT-Jack <78714499+BIT-Jack@users.noreply.github.com>
2024-09-20 13:10:24 +0800

    Update bash_training_and_test.sh

--- bash_training_and_test.sh
+++ bash_training_and_test.sh
@@ -5,16 +5,17 @@
 echo " Running Continual Training EXP"
 echo $(pwd)
 
-
+# setting --replayed_rc as 1 to record selected memory samples
 python train_CL.py  --model b2p --buffer_size 500 --debug_mode 0 --replayed_rc 1 & P1=$!
 wait $P1
+# setting --store_traj as True to record predicted trajectory for visualization 
 python test_CL.py --model b2p --buffer_size 500 --num_tasks 8 --store_traj True & P2=$!
 wait $P2
 echo "Ours Finished"
 
-python train_CL.py --model gem --buffer_size 500 --debug_mode 0 --replayed_rc 1 & P3=$!
+python train_CL.py --model gem --buffer_size 500 --debug_mode 0  & P3=$!
 wait $P3
-python test_CL.py --model gem --buffer_size 500 --num_tasks 8 --store_traj True & P4=$!
+python test_CL.py --model gem --buffer_size 500 --num_tasks 8  & P4=$!
 wait $P4
 echo "GEM Finished"
 
@@ -31,15 +32,15 @@ python test_CL.py --model der --buffer_size 500 --num_tasks 8 --store_traj True 
 wait $P8
 echo "DER Finished"
 
-python train_CL.py --model agem --buffer_size 500 --debug_mode 0 --replayed_rc 1 & P9=$!
+python train_CL.py --model agem --buffer_size 500 --debug_mode 0  & P9=$!
 wait $P9
-python test_CL.py --model agem --buffer_size 500 --num_tasks 8 --store_traj True & P10=$!
+python test_CL.py --model agem --buffer_size 500 --num_tasks 8  & P10=$!
 wait $P10
 echo "A-GEM Finished"
 
-python train_CL.py --model vanilla --buffer_size 0 --debug_mode 0 --replayed_rc 0 & P11=$!
+python train_CL.py --model vanilla --buffer_size 0 --debug_mode 0  & P11=$!
 wait $P11
-python test_CL.py --model vanilla --buffer_size 500 --num_tasks 8 --store_traj True & P12=$!
+python test_CL.py --model vanilla --buffer_size 500 --num_tasks 8  & P12=$!
 wait $P12
 echo "Vanilla Finished"
 
