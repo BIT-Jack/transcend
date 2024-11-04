5baa1dbb04cad822ddefc118f7b24a6be7909904
BIT-Jack <78714499+BIT-Jack@users.noreply.github.com>
2024-11-04 16:30:10 +0800

    Update der.py

--- cl_model/der.py
+++ cl_model/der.py
@@ -24,10 +24,14 @@ class Der(nn.Module):
         self.opt = Adam(self.net.parameters(), lr=self.args.lr)
         self.buffer = Buffer(self.args.buffer_size, self.device, self.NAME)
 
+        self.total_task_id = []
+
     def observe(self, inputs, labels, task_id=None):
         self.opt.zero_grad()
         outputs = self.net(inputs)
         log_lanescore, heatmap, heatmap_reg = outputs
+
+        
         
 
 
@@ -36,8 +40,12 @@ class Der(nn.Module):
 
 
         if not self.buffer.is_empty():
-            buf_inputs, buf_logits = self.buffer.get_data(
+            buf_inputs, buf_logits, sampled_task_id = self.buffer.get_data(
                 self.args.minibatch_size, transform=self.transform)
+            
+            self.total_task_id.append(sampled_task_id)
+            
+            
             buf_outputs = self.net(buf_inputs) 
             buf_log_lanescore, buf_heatmap_logits, buf_heatmap_reg = buf_outputs
             
@@ -55,4 +63,9 @@ class Der(nn.Module):
             self.buffer.add_data(examples=inputs, logits=heatmap.detach(), task_order=task_id)
         else:
             self.buffer.add_data(examples=inputs, logits=heatmap.detach())
-        return loss.item()
+
+
+        if task_id is not None:
+            return loss.item(), self.total_task_id
+        else:
+            return loss.item()
